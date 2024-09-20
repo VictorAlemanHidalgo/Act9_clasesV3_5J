eae20ec0348fb35a677d55badedcfcb9bb761672
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2024-09-20 09:32:24 -0600

    Add files via upload
    operadores aritmeticos en funciones de clases Python

---
+++ operadoresA.py
@@ -0,0 +1,71 @@
+print("Act 9 Clases V3")
+print("Victor Aleman, Matrícula: 22308051281123")
+# Zona de clases
+class Operadores1123:
+    # Zona de funciones
+    # Funcion de sumas
+    def suma1123(self, V, A):
+        S1123 = V + A
+        print(f"-La suma de {V} + {A} = {S1123}")
+    
+    # Funcion de restas
+    def resta1123(self, V, A):
+        R1123 = V - A
+        print(f"-La resta de {V} - {A} = {R1123}")
+
+    # Funcion de multiplicaciones
+    def multiplicacion1123(self, V, A):
+        Mu1123 = V * A
+        print(f"-La multiplicacion de {V} * {A} = {Mu1123}")
+
+    # Funcion de divisiones
+    def division1123(self, V, A):
+        D1123 = V / A
+        print(f"-La división de {V}/{A} = {D1123}")
+
+    # Funcion de modulos
+    def modulo1123(self, V, A):
+        Mo1123 = V % A
+        print(f"-El módulo de {V}%{A} = {Mo1123}")
+
+    # Funcion de elevaciones exponenciales o exponentes
+    def exponente1123(self, V, A):
+        E1123 = V ** A
+        print(f"-El exponenete de {V}**{A} = {E1123}")
+
+    # Funcion de cocientes
+    def cociente1123(self, V, A):
+        C1123 = V // A
+        print(f"-El cociente de {V}//{A} = {C1123}")
+
+# Zona de creación del objeto
+operandoVictor = Operadores1123()
+
+# Zona de uso de objetos
+# Mostrando una suma con una funcion
+print("---Función de la Suma---")
+operandoVictor.suma1123(5, 4)
+
+# Mostrando una resta con una funcion
+print("---Función de la Resta---")
+operandoVictor.resta1123(5, 4)
+
+# Mostrando una multiplicacion con una funcion
+print("---Función de la Multiplicación---")
+operandoVictor.multiplicacion1123(5, 4)
+
+# Mostrando una division con una funcion
+print("---Función de la División---")
+operandoVictor.division1123(5, 4)
+
+# Mostrando un modulo con una funcion
+print("---Función de la Módulo---")
+operandoVictor.modulo1123(5, 4)
+
+# Mostrando una exponente con una funcion
+print("---Función de la Exponente---")
+operandoVictor.exponente1123(5, 4)
+
+# Mostrando un cociente con una funcion
+print("---Función de la Cociente---")
+operandoVictor.cociente1123(5, 4)
